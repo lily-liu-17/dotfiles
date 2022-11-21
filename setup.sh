127c9797210a30e3b001fab3bb4ac0090a6d8982
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2022-11-20 23:25:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,65 +1,31 @@
 #!/bin/bash
-#
-# On a new AWS Debian Buster EC2 instance you will have to do the following before running this
-#
+
 # sudo apt update
 # sudo apt install -y git
-#
-# then ...
-#
+
 # git clone https://github.com/lily-liu-17/dotfiles.git
 # cd ./dotfiles && sh ./setup.sh
 #
 # update and upgrade system
-
 echo Update and upgrade system
 sudo apt-get update
 sudo apt-get full-upgrade -y
 
-# Configure Git
-git config --global user.email "lily.liu@mths.ca"
-git config --global user.name "lily-liu-17"
-git config --global init.defaultBranch main
-git config --global pull.rebase true
-git config --global fetch.prune true
-
-# Setup GitHub SSH keys
-echo "---------------------"
-echo "Press RETURN 3 times."
-ssh-keygen -t ed25519 -C "lily.liu@mths.ca"
-ssh-add ~/.ssh/id_ed25519
-echo ""
-cat ~/.ssh/id_ed25519.pub
-echo "Please copy and paste the above key into your GitHub SSH Keys."
-echo "(at: https://github.com/settings/keys)"
-echo "Once done, press RETURN."
-read  any_key
-echo ""
-echo "Next you will be asked if you want to continue"
-echo "Type 'yes' ..."
-echo "Once done, press any key."
-read  any_key
-ssh -T git@github.com
-
-# load some programs
-echo Load programs
 sudo apt install tree
 
-# load NVim
-# load requirements
 sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 # move back 1 directory
 cd ..
+# copy over NeoVim .config setup
+cp -r ~/dotfiles/.config ~/
 
-# load .bashrc file
 echo Load .bashrc file
 > ~/.bashrc
 cp ~/dotfiles/.bashrc ~/.bashrc
 
-# Setup TypeScript
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 # the following is added tp .bashrc, but we need to run it here
 export NVM_DIR="$HOME/.nvm"
@@ -114,6 +80,35 @@ mkdir ~/ICS4U/Unit3/Unit3-02
 mkdir ~/ICS4U/Unit3/Unit3-03
 mkdir ~/ICS4U/Unit3/Unit3-04
 mkdir ~/ICS4U/Unit3/Unit3-05
+
+# Configure Git
+git config --global user.email "Lily.liu@mths.ca"
+git config --global user.name "lily-liu-17"
+git config --global init.defaultBranch main
+git config --global pull.rebase true
+git config --global fetch.prune true
+
+# Setup GitHub SSH keys
+echo "---------------------"
+echo "Press RETURN 3 times."
+ssh-keygen -t ed25519 -C "Lily.liu@mths.ca"
+ssh-add ~/.ssh/id_ed25519
+echo ""
+cat ~/.ssh/id_ed25519.pub
+echo "Please copy and paste the above key into your GitHub SSH Keys."
+echo "(at: https://github.com/settings/keys)"
+echo "Once done, press RETURN."
+read  any_key
+echo ""
+echo "Next you will be asked if you want to continue"
+echo "Type 'yes' ..."
+echo "Once done, press any key."
+read  any_key
+ssh -T git@github.com
+
+# load GitHub CLI
+sudo apt install gh -y
+gh auth login
 
 # reboot
 echo ---
